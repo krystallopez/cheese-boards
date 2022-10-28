37c144515ea1c8f087c55639bb9b36c809284ceb
Krystal <lopezkrystal10@gmail.com>
2022-10-28 09:06:57 -0700

    created connection to sequelize

---
+++ db.js
@@ -0,0 +1,14 @@
+const { throws } = require("assert");
+const path = require("path");
+const Sequelize = require("sequelize");
+
+// Connection to db via sequelize
+const sequelize = new Sequelize({
+  dialect: "sqlite",
+  storage: path.join(__dirname, "db.sqlite"),
+  logging: false,
+});
+
+module.exports = {
+  sequelize,
+};
