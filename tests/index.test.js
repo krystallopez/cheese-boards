c3dde80c5336ea0ed6827941a5cb5f2ad778bbf0
Krystal <lopezkrystal10@gmail.com>
2022-10-28 10:23:19 -0700

    created test folder for readability again

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,5 +1,5 @@
 //test for index file
-const { DESCRIBE } = require("sequelize/types/query-types");
+//const { DESCRIBE } = require("sequelize/types/query-types");
 const { sequelize } = require("./db");
 const { User } = require("./models/index");
 const { seedUser } = require("./seedData");
@@ -8,11 +8,14 @@ const { seedUser } = require("./seedData");
 
 describe("User Model", () => {
   beforeAll(async () => {
+    // 'sync' method will create the tables based on the model class
+    // by setting 'force:true' the tables are recreated each time the test suite is run
     await sequelize.sync({ force: true });
   });
 
   test("can create a User", async () => {
-    const user1 = await User.create(seeUser[0]);
-    expect(user1.name).toEqual("Krystal");
+    const user = await User.create(seedUser[0]);
+    expect(user.name).toEqual("Krystal");
+    expect(user.email).toEqual("krystal@test.com");
   });
 });
