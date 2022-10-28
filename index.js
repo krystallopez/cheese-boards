ab593c2242290321c61689b43f20cd5e1e31dee3
Krystal <lopezkrystal10@gmail.com>
2022-10-28 09:11:09 -0700

    created index file and set up imports

---
+++ index.js
@@ -0,0 +1,10 @@
+// Define relationships here
+const { User } = require("./User.js");
+const { Boards } = require("./Boards.js");
+const { Cheese } = require("./Cheese.js");
+
+module.exports = {
+  User,
+  Boards,
+  Cheese,
+};
